7fbdabdec799f728d33186ff5adbfb8f0c905465
hironha <rafaelhirokato@gmail.com>
2022-10-20 21:03:06 -0300

    :lipstick: add white color to status bar

--- src/navigations/common/screenOptions.ts
+++ src/navigations/common/screenOptions.ts
@@ -3,12 +3,14 @@ import { type NativeStackNavigationOptions } from "@react-navigation/native-stac
 
 type DefaultScreenOptions = Pick<
   NativeStackNavigationOptions,
-  "headerTintColor" | "headerStyle" | "animation" | "animationDuration"
+  "headerTintColor" | "headerStyle" | "animation" | "animationDuration" | "statusBarStyle" | "statusBarColor"
 >;
 
 export const createDefaultScreenOptions = (theme: DefaultTheme): DefaultScreenOptions => {
   return {
     animation: "slide_from_right",
+    statusBarStyle: 'light',
+    statusBarColor: theme.colors.primary,
     animationDuration: 150,
     headerTintColor: theme.colors.white,
     headerStyle: { backgroundColor: theme.colors.primary },
